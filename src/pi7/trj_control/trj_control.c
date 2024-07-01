f17dc86da88c2d7b4e5028c896230b9ae7195747
danilodacca <danilopdacca@usp.br>
2024-07-01 13:55:46 -0300

    Cinemática inversa para int

--- src/pi7/trj_control/trj_control.c
+++ src/pi7/trj_control/trj_control.c
@@ -26,10 +26,10 @@ extern xQueueHandle qCommPIC1;
 extern xQueueHandle qCommPIC2;
 extern xQueueHandle qControlCommands;
 
-#define L1 17
-#define L2 20
-#define M_PI 3.14159265358979323846
-
+#define PI 31415
+#define L1 170
+#define L2 200
+#define SCALE 120
 
 //manda para o PIC um setpoint
 void tcl_generateSetpoint() {
@@ -52,32 +52,45 @@ void tcl_generateSetpoint() {
   printf("Setpoint: %f %f %f\n", line.x, line.y, line.z);
 
 
-  float L3= sqrt(pow(line.x,2)+pow(line.y,2));
-  int y = 33- line.y;
-  float beta = (180/M_PI)*acos((pow(L1,2)+pow(L2,2)-pow(L3,2))/(2*L1*L2));
-  float gamma=0;
-  float theta=0;
-  if (line.x==0){
-    gamma=90;
-  } else{
-    float theta=atan(y/line.x);
-    if(line.x<0){
-      gamma=180;
+  int L3 = round(sqrt(line.x * line.x + line.y * line.y));
+  int L3s = SCALE * L3;
+
+  // Calcular cos(beta) escalado
+  int L1s = L1 * SCALE;
+  int L2s = L2 * SCALE;
+  int L1_squared_scaled = L1s * L1s;
+  int L2_squared_scaled = L2s * L2s;
+  int L3_squared_scaled = L3s * L3s;
+  int cos_beta_scaled =(L1_squared_scaled + L2_squared_scaled - L3_squared_scaled) / (2 * L1 * L2); // OK
+
+  int beta_scaled = round((180 * 10000 / PI) * acos((float)cos_beta_scaled / (SCALE * SCALE)));
+  // Calcular gamma
+  int gamma = 0;
+  int thetas = 0;
+
+  if (line.x == 0) {
+    gamma = 90;
+  } else {
+      thetas = SCALE*atan((float)line.y / line.x);
+    if (line.x < 0) {
+      gamma = 180;
     }
   }
-  gamma+= (180/M_PI) * (theta - acos((pow(L1,2)+pow(L3,2)-pow(L2,2))/(2*L1*L3)));
+  
+  int cos_gamma_scaled = (L1_squared_scaled + L3_squared_scaled - L2_squared_scaled) / (2 * L1 * L3);
+  gamma += (180 * 10000 / PI) * ((float)thetas/SCALE - acos((float) cos_gamma_scaled / (SCALE*SCALE)));
 
   toPic1.SOT = (char)':';
-  toPic1.ADD = (char)'1';
+  toPic1.ADD = (char)'a';
   toPic1.COM = (char)'p';
   toPic1.VAL = gamma; // angulo quadril
   toPic1.EOT = (char)';';
   xQueueSend(qCommPIC1, &toPic1, portMAX_DELAY);
   
   toPic2.SOT = (char)':';
-  toPic2.ADD = (char)'2';
+  toPic2.ADD = (char)'b';
   toPic2.COM = (char)'p';
-  toPic2.VAL = beta; // angulo joelho
+  toPic2.VAL = beta_scaled;//; // angulo joelho
   toPic2.EOT = (char)';';
   xQueueSend(qCommPIC2, &toPic2, portMAX_DELAY);
 
