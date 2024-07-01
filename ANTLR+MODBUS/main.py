d845f2f5fbe59b193f61b50a3223d04bed0777ea
Victor Nascimento Pereira <caesarvictor@usp.br>
2024-06-30 21:45:09 -0300

    monta matriz a partir de entrada Gcode

--- ANTLR+MODBUS/main.py
+++ ANTLR+MODBUS/main.py
@@ -3,27 +3,36 @@ from GCodeLexer import GCodeLexer
 from GCodeParser import GCodeParser
 from GCodeListener import GCodeListener
 
-class WalkListener(GCodeListener):
-    def enterStatement(self, ctx):
-        if ctx.codfunc() is not None:
-            print(ctx.codfunc().getText())
-        if ctx.coordx() is not None:
-            print(ctx.coordx().coord().getText())
-        if ctx.coordy() is not None:
-            print(ctx.coordy().coord().getText())
 
+
+class WalkListener(GCodeListener):
+    def __init__(self):
+        self.coordenadas = []
+        self.set = False
+        self.x = 0
+        self.y = 0
+
+    def enterStatement(self, ctx):
+        if ctx.coordx() is not None:
+            self.x = int(ctx.coordx().coord().getText())
+            set = True
+        if ctx.coordy() is not None:
+            self.y = int(ctx.coordy().coord().getText())
+            set = True
+        if set:
+            self.coordenadas.append((self.x, self.y))
 
 def main():
-    with open("GCode-example") as file:
+    with open("ANTLR+MODBUS/GCode-example") as file:
         data = f'{file.read()}'
     lexer = GCodeLexer(InputStream(data))
     stream = CommonTokenStream(lexer)
     parser = GCodeParser(stream)
     tree = parser.gcode()
-    print(tree.toStringTree(recog=parser))
     listener = WalkListener()
     walker = ParseTreeWalker()
     walker.walk(listener, tree)
+    print(listener.coordenadas)
 
 if __name__ == '__main__':
     main()
